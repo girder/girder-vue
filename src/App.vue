c2d2f3843d1dc5fdc2c889b121ed4266b15ebb18
Zach Mullen <zach.mullen@kitware.com>
2018-02-07 11:55:49 -0500

    Some layout elements

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
 #app
-  img(src="./assets/logo.png")
   router-view
 </template>
 
@@ -13,12 +12,12 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
+  a
+    cursor pointer
 </style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,31 +1,30 @@
 <template lang="pug">
 div
-  div {{ msg }}
+  ul
+    li #[router-link(to="collections") Collections]
+    li #[router-link(to="users") Users]
+    li #[router-link(to="groups") Groups]
+    li #[router-link(to="admin", v-if="isAdmin") Admin console]
+  div
+    a(v-if="isLoggedIn") {{ user.login }}
+    div(v-else)
+      a(@click="registerDialog") Register
+      | or
+      a(@click="loginDialog") Login
   router-view
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex'
+
 export default {
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App!'
-    }
+  computed: {
+    ...mapState('auth', ['user']),
+    ...mapGetters('auth', ['isLoggedIn', 'isAdmin'])
+  },
+  methods: {
+    registerDialog: () => { console.log ('register!') },
+    loginDialog: () => { console.log ('login!') }
   }
 }
 </script>
-
-<style lang="stylus" scoped>
-h1, h2
-  font-weight normal
-
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
-</style>
