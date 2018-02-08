a5cba06f426b5ef6a0c9513cb53dea5c80cd7984
Zach Mullen <zach.mullen@kitware.com>
2018-02-08 10:46:07 -0500

    Build a DLL manifest for runtime linking with core library

--- README.md
+++ README.md
@@ -8,8 +8,8 @@
 # install dependencies
 npm install
 
-# serve with hot reload at localhost:8080
-npm run dev
+# serve with hot reload
+npm start
 
 # build for production with minification
 npm run build
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,7 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: ['./src/main.js']
   },
   output: {
     path: config.build.assetsRoot,
@@ -32,7 +32,7 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
