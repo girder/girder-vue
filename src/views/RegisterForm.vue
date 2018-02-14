d8abbbf523f75dbd9fc4a0ab09f1c43663432f05
Zach Mullen <zach.mullen@kitware.com>
2018-02-14 15:00:56 -0500

    Cleanup duplication in RegisterForm

--- src/views/RegisterForm.vue
+++ src/views/RegisterForm.vue
@@ -4,11 +4,11 @@ v-card
     h3.headline Register
   form(@submit.prevent="register")
     v-card-text
-      v-text-field(v-model="username", label="Username", autofocus, :error-messages="usernameErrors")
-      v-text-field(v-model="email", label="Email", :error-messages="emailErrors")
-      v-text-field(v-model="firstName", label="First name", :error-messages="firstNameErrors")
-      v-text-field(v-model="lastName", label="Last name", :error-messages="lastNameErrors")
-      v-text-field(v-model="password", type="password", label="Password", :error-messages="passwordErrors")
+      v-text-field(v-model="username", label="Username", autofocus, :error-messages="listify(errors.login)")
+      v-text-field(v-model="email", label="Email", :error-messages="listify(errors.email)")
+      v-text-field(v-model="firstName", label="First name", :error-messages="listify(errors.firstName)")
+      v-text-field(v-model="lastName", label="Last name", :error-messages="listify(errors.lastName)")
+      v-text-field(v-model="password", type="password", label="Password", :error-messages="listify(errors.password)")
       v-text-field(v-model="retypePassword", type="password", label="Retype password", :error-messages="retypePasswordErrors")
     v-card-actions
       v-btn(type="submit", color="primary", :disabled="submitDisabled", :loading="registerInProgress") Register
@@ -38,21 +38,6 @@ export default {
     }
   },
   computed: {
-    usernameErrors () {
-      return this.errors.login ? [this.errors.login] : []
-    },
-    emailErrors () {
-      return this.errors.email ? [this.errors.email] : []
-    },
-    firstNameErrors () {
-      return this.errors.firstName ? [this.errors.firstName] : []
-    },
-    lastNameErrors () {
-      return this.errors.lastName ? [this.errors.lastName] : []
-    },
-    passwordErrors () {
-      return this.errors.password ? [this.errors.password] : []
-    },
     submitDisabled () {
       return !!this.retypePasswordErrors.length || this.registerInProgress
     },
@@ -81,7 +66,8 @@ export default {
       } else {
         this.retypePasswordErrors = ['Passwords do not match']
       }
-    }
+    },
+    listify: (v) => v ? [v] : []
   }
 }
 </script>
